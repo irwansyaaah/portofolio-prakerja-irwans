aecfa1f5ac5adf75a6000aa1e5606a7071c938ea
irwansyah <irwansyah769@gmail.com>
2023-06-22 16:38:13 +0700

    commit message

---
+++ script/index.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+  
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    if (body.classList.contains("dark-mode")) {
+      darkModeToggle.checked = true;
+    } else {
+      darkModeToggle.checked = false;
+    }
+} onclick()
+  
